68a13a90ce0353e7d2a827092f75756b4b4d3847
andreykuluev96 <andreykuluev96@omo.systems>
2020-08-18 17:39:52 +0300

    15

---
+++ GoogleTests/h/program.h
@@ -0,0 +1,10 @@
+#ifndef GTEST_PROGRAM_H_
+#define GTEST_PROGRAM_H_
+
+// Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n);
+
+// Returns true if and only if n is a prime number.
+bool IsPrime(int n);
+
+#endif // GTEST_PROGRAM_H_
