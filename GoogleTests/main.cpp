31d52f4207d2176c6095fc1cec66f1d0e37ed40d
andreykuluev96 <andreykuluev96@omo.systems>
2020-08-18 17:47:54 +0300

    xcxc

--- GoogleTests/main.cpp
+++ GoogleTests/main.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
-#include <iomanip>
-#include <string.h>
-
-int main()
-{
-
-    return 0;                                                  
+#include "gtest/gtest.h"
+ 
+double square_root (double num) 
+{ 
+    if (num < 0.0) { 
+        std::cerr << "Error: Negative Input\n";
+        exit(-1);
+    }
+    // Код для 0 и положительных чисел…
 }
-
+ 
+TEST (SquareRootTest, ZeroAndNegativeNos) 
+{ 
+    ASSERT_EQ(0.0, square_root(0.0));
+    ASSERT_EXIT(square_root(-22.0), ::testing::ExitedWithCode(-1), "Error: Negative Input");
+}
+ 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
