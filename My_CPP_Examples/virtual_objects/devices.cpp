df9a2e0f4c918d307e770c7aca2b7f1e0b96ce35
andreykuluev96 <andreykuluev96@omo.systems>
2020-08-28 15:55:56 +0300

    sdsdsdd

--- My_CPP_Examples/virtual_objects/devices.cpp
+++ My_CPP_Examples/virtual_objects/devices.cpp
@@ -16,18 +16,24 @@ uint64_t Base_device::get_mac(void)
     return mac;
 }
 
+void Base_device::set_command(eCommand cmd)
+{
+    Command = cmd;
+
+}
+
 // Intrercom
 Intercom::Intercom()
 {
 
 }
 
-void Intercom::set_command(int cmd)
+void Intercom::set_command(Base_device::eCommand cmd)
 {
-    Command = cmd;
+    // to do
 }
 
-int Intercom::get_command(void)
+Base_device::eCommand Intercom::get_command(void)
 {
     return Command;
 }
@@ -38,12 +44,12 @@ Curtain::Curtain()
 
 }
 
-void Curtain::set_command(int cmd)
+void Curtain::set_command(Base_device::eCommand cmd)
 {
-    Command = cmd;
+    // to do
 }
 
-int Curtain::get_command(void)
+Base_device::eCommand Curtain::get_command(void)
 {
     return Command;
 }
--- My_CPP_Examples/virtual_objects/devices.hpp
+++ My_CPP_Examples/virtual_objects/devices.hpp
@@ -5,40 +5,44 @@
 class Base_device
 {
 	public:
+		enum class eCommand: uint8_t {UNDEFINED, ON, OFF, OPEN, CLOSE};
+
 		Base_device();
 		~Base_device();
 
 		uint64_t get_mac(void);
 		void set_mac(uint64_t mac);
 		
-		virtual void set_command(int cmd) = 0;
-		virtual int get_command(void) = 0;
+		void set_command(eCommand cmd);
+		virtual void set_command_proccesing() = 0;
+		
+		virtual eCommand get_command(void) = 0;
+
 	private:
 		uint64_t mac;
+		eCommand Command = eCommand::UNDEFINED;
 };
 
 class Intercom: public Base_device 
 {
 	private:
-		//enum eCommand {UNDEFINED, ON, OFF} Command = Intercom::eCommand::UNDEFINED;
-		int Command;
+		
 	public:
 		Intercom();
 		~Intercom();
 		
-		void set_command(int cmd) override;
-		int get_command(void) override;
+		void set_command_proccesing() override;
+		Base_device::eCommand get_command(void) override;
 };
 
 class Curtain: public Base_device 
 {
 	private:
-		//enum eCommand {UNDEFINED, OPEN, CLOSE} Command = Curtain::eCommand::UNDEFINED;
-		int Command;
+
 	public:
 		Curtain();
 		~Curtain();
 		
-		void set_command(int cmd) override;
-		int get_command(void) override;
+		void set_command_proccesing() override;
+		Base_device::eCommand get_command(void) override;
 };
