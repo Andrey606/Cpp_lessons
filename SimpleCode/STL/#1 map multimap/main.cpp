defa3f091fa57328324135c91e39456df2efff21
Andrey606 <andreykuluev96@gmail.com>
2020-12-11 12:23:26 +0200

    sd

---
+++ SimpleCode/STL/#1 map multimap/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+// MAP | MULTIMAP | Ассоциативные контейнеры |
+
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+ 
+    
+  
+    return 0;
+}
