f5e95f7c38396727c164bb193d5ba42f3d8e595d
Andrey606 <andreykuluev96@gmail.com>
2021-04-27 19:04:44 +0300

    sdsd

---
+++ Stepic/Course_2/Level1.6 (указатели на методы)/task1 (указатель на методы)/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+
+Напишите возвращающую bool шаблонную функцию compare, которая принимает две константные ссылки на объекты 
+одного типа и указатель на константный метод этого типа без параметров, который в свою очередь возвращает 
+значение какого-то второго типа. Функция должна сравнивать объекты по значениям, которые для них вернёт соответствующий метод, 
+и возвращать true, если значение для первого объекта оказалось меньше, чем для второго.
+
+Пример использования функции compare:
+
+std::string s1("Elf");
+std::string s2("Archer");
+
+// сравнение строк по длине
+bool r1 = compare(s1, s2, &std::string::size); // true
+bool r2 = compare(s1, s1, &std::string::size); // false
+
+*/
+
+template<typename T, class U>
+bool compare(const T &a, const T &b, U (T::*mptr)() const )
+{
+    return ((a.*mptr)() < (b.*mptr)());
+}
+
+
+int main(int argc, const char * argv[]) 
+{
+    std::string s1("Elf");
+    std::string s2("Archer");
+
+    bool r1 = compare(s1, s2, &std::string::size); cout<< r1 <<endl; // true
+    bool r2 = compare(s1, s1, &std::string::size); cout<< r2 <<endl; // false
+
+    return 0;
+}
