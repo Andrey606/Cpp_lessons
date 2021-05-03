28d26e63edda7be3e93a7ea9bfdf94e9a9b5284b
Andrey606 <andreykuluev96@gmail.com>
2021-05-03 16:56:51 +0300

    asds

--- Stepic/Course_2/Level2.1 (c++11 & c++14)/переиспользование конструктора базового класса/main.cpp
+++ Stepic/Course_2/Level2.1 (c++11 & c++14)/переиспользование конструктора базового класса/main.cpp
@@ -8,10 +8,17 @@ public:
     Base(const  Base&) = delete; // в новом стандарте можно удалять конструктор копирование
     Base() = default;
     Base(int a, float b): _a(a), _b(b){}
+
+    virtual void someMethod() final; //  final запрещает переопределять этот метод в производных классах
 private:
     int _a = 5; // в новом стандарте можно инициализировать переменные по дефолту тут
     float _b = 6.6;
 };
+
+void Base::someMethod()
+{
+
+}
 
 class SomeClass : public Base
 {
