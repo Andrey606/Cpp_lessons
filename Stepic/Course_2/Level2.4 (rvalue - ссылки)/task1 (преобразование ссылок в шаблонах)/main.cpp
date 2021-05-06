1663923517b6fffc7d4c6f900efabb77298de557
Andrey606 <akuluev@ukr.net>
2021-05-06 03:00:59 +0300

    sds

--- Stepic/Course_2/Level2.4 (rvalue - ссылки)/task1 (преобразование ссылок в шаблонах)/main.cpp
+++ Stepic/Course_2/Level2.4 (rvalue - ссылки)/task1 (преобразование ссылок в шаблонах)/main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 /*
     правила склеек ссылок 
 
-    * T& & -> T&
-    * T& && -> T&
-    * T&& & -> T&
-    * T&& && -> T&&
+    * T& + & -> T&
+    * T& + && -> T&
+    * T&& + & -> T&
+    * T&& + && -> T&&
+    
+    * T + && -> T&&
 */
 
+// * если вызов foo от lvalue типа A, то Т = A& -> (A& + && = A&)
+// * если вызов foo от rvalue типа A, то Т = A -> (A + && = A&&)
 template<typename T>
 void foo(T && t)
 {
